8c4ff0e8e53c4e1c77144230398935e8bf5a4262
aminaltory <55554272+aminaltory@users.noreply.github.com>
2020-02-18 16:25:28 +0200

    UUID

---
+++ UUID.py
@@ -0,0 +1,13 @@
+import random as r
+
+def generate_uuid():
+        random_string = ''
+        random_str_seq = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        uuid_format = [8, 4, 4, 4, 12]
+        for n in uuid_format:
+            for i in range(0,n):
+                random_string += str(random_str_seq[r.randint(0, len(random_str_seq) - 1)])
+            if n != 12:
+                random_string += '-'
+        return random_string
+print(generate_uuid())
