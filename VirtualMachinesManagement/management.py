838623110ffa8504ac724dff13c65e075e61f66c
islamab123 <frnsheska@gmail.com>
2020-02-18 20:54:09 +0200

    update  management

--- VirtualMachinesManagement/management.py
+++ VirtualMachinesManagement/management.py
@@ -4,10 +4,10 @@ import pyVmomi
 import pyvim
 import uuid
 import connection
-
+import LogIn
 
 class Management:
-    def __init__(self, VMName, Ip, OperatingSystem, Status, Ram, Storage, LifeTime):
+    def __init__(self, VMName, Ip, OperatingSystem, Status, Ram, Storage, LifeTime,UniqueiD):
         self.VMName = VMName
         self.Ip = Ip
         self.OperatingSystem = OperatingSystem
@@ -15,17 +15,16 @@ class Management:
         self.Ram = Ram
         self.Storage = Storage
         self.LifeTime = LifeTime
+        self.UniqueiD=UniqueiD
     def LogIn(self,UserName,Password):
-        #if  user name and password are true in the data base
-        #return UniqueiD /and save it in sql
-        pass
+       LogIn.LogeIn().CheckLogIn(UserName,Password)
     def CreateVM_(self):
         # check if the vm name already exist if not then
         # check if the vm data exist in the esxi
         # if true the create vm
+        #create Log in
         # save on SQL
         pass
-
     def DeleteVM_(self,VMid,host):
             DB.VMDB().deleteFromVMDB(VMid)  # delete by vm UniqueiD
             print("VM Deleted from SQL")
@@ -34,44 +33,51 @@ class Management:
                 print("vm Dose not Exist")
             else:
                 print("VM Deleted from Esxi 6.5")
-    def FindVMID(self,VMName,UniqueiD):
-         connection.connections() #connect to data base
-         ListOfVMs=[]
-         ListOfVMs=DB.GetAllVMsForThisUser(UniqueiD)
-       #  for i in ListOfVMs:
-             #sent to def (i[2])
-         #  DB.VMDB().Search(UniqueiD)#searsh by UniqueiD
-          #if exist
-          # return ip_address
-    def GetAllVMs(self,content):
+    def GetAllVMsFromSQL(self,PassWord,VMName):#only from SQL
+        ListOfUniqueiD=[]
+        ListOfAllVMs=[]
+        if(LogIn.LogeIn().CheckLogIn('root',PassWord)):
+           listUsers = LogIn.LogeIn().AllUsers()
+           for i in listUsers:
+               ListOfUniqueiD.append(i[2])
+        ListOfAllVMs=DB.VMDB().fetchAllVMData()
+        for i in ListOfAllVMs:
+            for j in ListOfUniqueiD:
+               if(i[8]==j and VMName==i[1]):#i[8]==> UniqueiD ,i[1]==> VM name
+                   print(i)
+                   return i
+    def checkGetAllVMs(self):
+        c=ESXI.ManageESXI().ConnectEsxi()
+        dicVM=ESXI.ManageESXI().Get_All_VM(c)
         ListVMDB = []
-        ListVMEsxi={}
-        newlist=[]
-        ListVMDB=DB.VMDB().fetchAllVMData()
-        ListVMEsxi=ESXI.ManageESXI().Get_All_VM(content)
-       # print("all the VM's that have been saved on the Data Base SQL : ")
+        newlist = []
+        ListVMDB = DB.VMDB().fetchAllVMData()
+        # print("all the VM's that have been saved on the Data Base SQL : ")
         if (len(ListVMDB) < 1):
             print("this user have no VM's in the Data Base")
-        if (ListVMEsxi == None):
+        if (dicVM == None):
             print("there is no vms in the esxi to thes user")
-        if (len(ListVMDB) > 1 and ListVMEsxi != None):
-            for keys, values in ListVMEsxi.items():
-               # print(keys, " : ", values)
+        if (len(ListVMDB) > 1 and dicVM != None):
+            for keys, values in dicVM.items():
+                # print(keys, " : ", values)
                 if (keys == 'ip_address'):
-                    newlist.append(values)#all the vm ip from the  dictionary that in esxi 6,5 in a new list
-            counter=0
+                    newlist.append(values)  # all the vm ip from the  dictionary that in esxi 6,5 in a new list
+            counter = 0
             for i in ListVMDB:
                 for j in newlist:
-                    if(i[3]==j):
-                        counter=counter+1
-            if (len(ListVMDB)==counter):
+                    if (i[3] == j):
+                        counter = counter + 1
+            if (len(ListVMDB) == counter):
                 return True
             else:
                 return False
+    def GetAllVMs(self,PassWord,VMName):
+        if(self.checkGetAllVMs()):# not shore
+            self.GetAllVMsFromSQL(PassWord,VMName)
+    def ExtendVMLifeTime(self,PassWord,VMName):
+        DB.VMDB().ExtendLifeTime(PassWord,VMName)
 
-          #  for()
-    def ExtendVMLifeTime(self,Name, VMIP, OS, LifeTime, Owner, Designation):
-         DB.ExtendLifeTime(Name, VMIP, OS, LifeTime, Owner, Designation)
+
 #c=ESXI.ManageESXI().ConnectEsxi()
 #dec=ESXI.ManageESXI().Get_All_VM(c)
 #m=Management("vm457",12345678,"XP","on",'1','','')#'VMName', 'Ip', 'OperatingSystem', 'Status', 'Ram', 'Storage', and 'LifeTime
